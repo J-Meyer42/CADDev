99c8ccaba2ee6609ef11c22949f4229ab9bc7330
Opalfuren <42529357+Opalfuren@users.noreply.github.com>
2018-08-20 00:06:47 -0400

    Second line

---
+++ esx_holdup/config.lua
@@ -0,0 +1,119 @@
+Config = {}
+Config.Locale = 'en'
+
+Stores = {
+	["sandyshores_twentyfoursever"] = {
+		position = { ['x'] = 1961.24682617188, ['y'] = 3749.46069335938, ['z'] = 32.3437461853027 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Sandy Shores)",
+		lastrobbed = 0
+	},
+	["yellowjack_bar"] = {
+		position = { ['x'] = 1981.5603027344, ['y'] = 3051.0554199219, ['z'] = 47.214988708496 },
+		reward = math.random(100,1000),
+		nameofstore = "Yellow Jack. (Sandy Shores)",
+		lastrobbed = 0
+	},
+	["ocean_liquor"] = {
+		position = { ['x'] = -2959.33715820313, ['y'] = 388.214172363281, ['z'] = 14.0432071685791 },
+		reward = math.random(100,1000),
+		nameofstore = "Robs Liquor. (Great Ocean Higway)",
+		lastrobbed = 0
+	},
+	["sanandreas_liquor"] = {
+		position = { ['x'] = -1219.85607910156, ['y'] = -916.276550292969, ['z'] = 11.3262157440186 },
+		reward = math.random(100,1000),
+		nameofstore = "Robs Liquor. (San Andreas Avenue)",
+		lastrobbed = 0
+	},
+	["grove_ltd"] = {
+		position = { ['x'] = -43.4035377502441, ['y'] = -1749.20922851563, ['z'] = 29.421012878418 },
+		reward = math.random(100,300),
+		nameofstore = "LTD Gasoline. (Grove Street)",
+		lastrobbed = 0
+	},
+	["mirror_ltd"] = {
+		position = { ['x'] = 1160.67578125, ['y'] = -314.400451660156, ['z'] = 69.2050552368164 },
+		reward = math.random(100,300),
+		nameofstore = "LTD Gasoline. (Mirror Park Boulevard)",
+		lastrobbed = 0
+	},
+	["littleseoul_twentyfourseven"] = {
+		position = { ['x'] = -709.17022705078, ['y'] = -904.21722412109, ['z'] = 19.215591430664 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Little Seoul)",
+		lastrobbed = 0
+	},
+	["strawberry_twentyfourseven"] = {
+		position = { ['x'] = 29.64714, ['y'] = -1339.3862, ['z'] = 29.497049 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Strawberry)",
+		lastrobbed = 0
+	},
+	["elrancho_liquor"] = {
+		position = { ['x'] = 1126.5953369141, ['y'] = -980.80041503906, ['z'] = 45.415637969971 },
+		reward = math.random(100,1000),
+		nameofstore = "Robs Liquor. (El Rancho)",
+		lastrobbed = 0
+	},
+	["prosperity_liquor"] = {
+		position = { ['x'] = -1479.0913085938, ['y'] = -374.54071044922, ['z'] = 39.163276672363 },
+		reward = math.random(100,1000),
+		nameofstore = "Robs Liquor. (Prosperity)",
+		lastrobbed = 0
+	},
+	["rockford_ltd"] = {
+		position = { ['x'] = -1828.7294921875, ['y'] = 798.89880371094, ['z'] = 138.1820526123 },
+		reward = math.random(100,300),
+		nameofstore = "LTD Gasoline. (Rockford)",
+		lastrobbed = 0
+	},
+	["route68_twentyfourseven"] = {
+		position = { ['x'] = 545.84405517578, ['y'] = 2663.2875976563, ['z'] = 42.156494140625 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Route 68)",
+		lastrobbed = 0
+	},
+	["senorasandy_twentyfourseven"] = {
+		position = { ['x'] = 2673.4084472656, ['y'] = 3286.0549316406, ['z'] = 55.24116897583 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Senora Fwy near Sandy Shores)",
+		lastrobbed = 0
+	},
+	["senorapaleto_twentyfourseven"] = {
+		position = { ['x'] = 1734.1329345703, ['y'] = 6420.5454101563, ['z'] = 35.037227630615 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Senora Fwy near Paleto Bay)",
+		lastrobbed = 0
+	},
+	["clinton_twentyfourseven"] = {
+		position = { ['x'] = 377.63040161133, ['y'] = 332.86743164063, ['z'] = 103.56636810303 },
+		reward = math.random(100,200),
+		nameofstore = "24/7. (Clinton)",
+		lastrobbed = 0
+	},
+	["grapeseed_ltd"] = {
+		position = { ['x'] = 1707.4349365234, ['y'] = 4920.021484375, ['z'] = 42.063671112061 },
+		reward = math.random(100,300),
+		nameofstore = "LTD Gasoline. (Grapeseed)",
+		lastrobbed = 0
+	},
+	["tequilala_bar"] = {
+		position = { ['x'] = -562.25860595703, ['y'] = 287.28088378906, ['z'] = 82.176475524902 },
+		reward = math.random(200,2000),
+		nameofstore = "Tequi-La-La Bar. (Eclipse)",
+		lastrobbed = 0
+	},
+	["vanillaunicorn_bar"] = {
+		position = { ['x'] = 128.41259765625, ['y'] = -1285.6651611328, ['z'] = 29.279972076416 },
+		reward = math.random(200,2500),
+		nameofstore = "Vanilla Unicorn. (Elgin)",
+		lastrobbed = 0
+	},
+	["TheBusniess"] = {
+		position = { ['x'] = -615.62408447266, ['y'] = -1623.3623046875, ['z'] = 33.01053237915 },
+		reward = math.random(200,2500),
+		nameofstore = "TheBusniess MutinyRD & South Arsenal ST",
+		lastrobbed = 0
+	}	
+}
